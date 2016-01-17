04fd91dde8ce0be347186784342ef7d5314bbc1c
JemarJones <jemarkjones@gmail.com>
2016-01-17 02:12:32 -0500

    initially implemented room functionality

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,34 @@ module.exports.getRouter = function(io){
 		res.render('index.html');
 	});
 
-	io.on('connection', function(socket){
 
+	var activeRooms = {'room0': generateRoom() };
+	var roomIdCount = 0;
+	var userCount = 0;
+	var initialPlacableCellAmount = 30;
+
+	io.sockets.on('connection', function(socket){
+
+		socket.on('joinRoom',function(data){
+			socket.data = {};
+			socket.data.id = userCount++;
+			socket.data.colour = data.colour;
+			socket.data.cellsLeft = initialPlacableCellAmount;
+			socket.data.readyToPlay = false;
+			if (activeRooms['room' + roomIdCount].species.length == 2 ){
+				activeRooms['room' + ++roomIdCount] = generateRoom();
+			}
+			var assignedRoom = 'room' + roomIdCount;
+			socket.join(assignedRoom);
+			socket.data.room = assignedRoom;
+			activeRooms[assignedRoom].species[species.length] = socket.data;
+			socket.emit('roomJoined',activeRooms[assignedRoom]);
+			socket.broadcast.to(assignedRoom).emit('newSpeciesJoined',activeRooms[assignedRoom]);
+		});
 	});
 
+	function generateRoom(){
+		return {species: []};
+	}
 	return router;
 };
